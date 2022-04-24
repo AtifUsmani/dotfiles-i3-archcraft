dde49b2ce8c4a4642f8f8be27533cda41aca0910
Bunta Fujiwara <73231217+AtifUsmani@users.noreply.github.com>
2022-04-24 20:27:51 +0530

    Initial Commit

---
+++ .config/i3/bin/autostart.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+## Autostart Programs
+
+# Kill already running process
+_ps=(picom dunst ksuperkey mpd xfce-polkit xfce4-power-manager)
+for _prs in "${_ps[@]}"; do
+	if [[ `pidof ${_prs}` ]]; then
+		killall -9 ${_prs}
+	fi
+done
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# Polkit agent
+/usr/lib/xfce-polkit/xfce-polkit &
+
+# Enable power management
+xfce4-power-manager &
+
+# Enable Super Keys For Menu
+ksuperkey -e 'Super_L=Alt_L|F1' &
+ksuperkey -e 'Super_R=Alt_L|F1' &
+
+# Restore wallpaper
+#hsetroot -cover ~/Pictures/wall/wallpaperflare.com_wallpaper2.jpg
+wal -i ~/Pictures/wall/wallpaperflare.com_wallpaper2.jpg
+
+# Lauch notification daemon
+~/.config/i3/bin/i3dunst.sh
+
+# Lauch polybar
+# ~/.config/i3/bin/i3bar.sh
+~/.config/polybar/launch.sh
+
+# Lauch compositor
+~/.config/i3/bin/i3comp.sh
+
+# Start mpd
+exec mpd &
